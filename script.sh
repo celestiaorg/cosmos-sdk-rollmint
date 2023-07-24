b9e5760b2cebab9cdc13771f5cbff61585e7ce6c
Connor O'Hara <connor@switchboard.xyz>
2023-07-24 17:35:22 +0200

    try to fix gm builder

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 apt update
 apt install jq -y
 curl https://get.ignite.com/cli@v0.26.1! | bash
 ignite scaffold chain gm --address-prefix gm
 cd gm
-go mod edit -replace github.com/cosmos/cosmos-sdk=../cosmos-sdk
+go mod edit -replace github.com/cosmos/cosmos-sdk=./cosmos-sdk
 go mod edit -replace github.com/tendermint/tendermint=github.com/rollkit/cometbft@v0.0.0-20230524013049-75272ebaee38
 go mod tidy
 go mod download
